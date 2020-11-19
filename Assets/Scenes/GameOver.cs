08f2da4e9ceb180417b03ea72475f388dae70b71
Zachary Stroven <zacstroven@gmail.com>
2020-11-19 17:14:34 -0500

    Add files via upload

---
+++ Assets/Scenes/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    Text score;
+    public string newGameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       score = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        ScoreScript.scoreValue = 0;
+
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
