33e93f2b3e64c03ae58fb2bd167a539d5ad790e6
Zachary Stroven <zacstroven@gmail.com>
2020-11-19 17:09:05 -0500

    Add files via upload

---
+++ Assets/HighScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public static int highScore;
+    Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreScript.scoreValue > highScore)
+        {
+            highScore = ScoreScript.scoreValue;
+        }
+        score.text = "high score: " + highScore;
+    }
+}
